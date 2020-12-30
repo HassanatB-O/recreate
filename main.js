e7f95772fd296d20ad232bad76c1e5db0347c4a3
HassanatB-O <60980025+HassanatB-O@users.noreply.github.com>
2020-12-30 12:43:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function callAjax(){
     // Creating the XMLHttpRequest object
     var request = new XMLHttpRequest();
     // Instantiating the request object
-    request.open("GET", "https://opentdb.com/api.php?amount=10&type=boolean");
+    request.open("GET", "https://api.abalin.net/today");
     // Defining event listener for readystatechange event
     request.onreadystatechange = function() {
         // Check if the request is complete and was successful
