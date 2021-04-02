b0468c92910308a0cb6d0f89b5457db96df3f847
Hassanat Busari <60980025+HassanatB-O@users.noreply.github.com>
2021-04-02 11:17:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-function callAjax(){
-    // Creating the XMLHttpRequest object
-    var request = new XMLHttpRequest();
-    // Instantiating the request object
-    request.open("GET", "https://api.abalin.net/today");
-    // Defining event listener for readystatechange event
-    request.onreadystatechange = function() {
-        // Check if the request is complete and was successful
-        if (this.readyState === 4 && this.status === 200){
-            //Inserting the response from server into an HTML element 
-            document.getElementById('result').innerHTML = this.responseText;
-        }
-    };
-    // Sending the request to the server
-    request.send();
+
+const axios = require("axios");
+const URL = "https://otx.alienvault.com/api"
+async function callAjax(){
+    try{
+        const result = await axios.get(URL)
+        console.log("hi");
+        return result.data;
+    }
+    catch(err){
+        return err.message;
+    }
+    
+}
+const getCall = async () =>{
+    console.log(await callAjax())
 }
 
+getCall().then(res => console.log(res)).catch(err => console.log(err))
